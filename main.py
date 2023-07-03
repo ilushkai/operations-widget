9d73a15d10c6942faa68b7f82aaa187806856a52
Ilya Ignatenko <ilya.ilya.ignatenko@gmail.com>
2023-07-03 22:55:26 +1000

    project

---
+++ main.py
@@ -0,0 +1,13 @@
+from utils import *
+
+file = 'operations.json'
+
+sorted_f = dict_oper(file)
+
+k = sort(sorted_f)
+
+for i in k:
+    print(i['date'], i['description'])
+    print(format_from_number(i['from']), '->', format_to_number(i['to']))
+    print(i['operationAmount']['amount'], i['operationAmount']['currency']['name'])
+    print('')
